de21ed272bfcd414f27f941a610b0d22d08ed9ba
elizaveta.evgenia <elizaveta.evgenia@mail.ru>
2026-07-09 01:33:00 +0700

    Юнит-тесты на координатор

---
+++ Tests/AppCoordinatorTests.swift
@@ -0,0 +1,26 @@
+//
+//  AppCoordinatorTests.swift
+//  CarRental
+//
+//  Created by Elizaveta Evgenia on 09.07.2026.
+//
+
+import XCTest
+@testable import CarRental
+
+@MainActor
+final class AppCoordinatorTests: XCTestCase {
+    func testInitialRouteIsCarList() {
+        let coordinator = AppCoordinator()
+
+        XCTAssertEqual(coordinator.route, .carList)
+    }
+
+    func testMakeCarListViewModelProvidesTitle() {
+        let coordinator = AppCoordinator()
+
+        let viewModel = coordinator.makeCarListViewModel()
+
+        XCTAssertEqual(viewModel.title, "Аренда машин")
+    }
+}
